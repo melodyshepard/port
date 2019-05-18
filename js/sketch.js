6c9b7029e9e1b63f7dbfc2fffbf930e51e21bec3
melodyshepard <47006519+melodyshepard@users.noreply.github.com>
2019-05-18 01:14:53 -0700

    yes ye syes

---
+++ js/sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  background(220);
+  background(188, 39, 16);
+stroke(224, 115, 71);
+  fill(130, 25, 9);
+  ellipse(400, 400, 500, 500);
+  translate(mouseX, mouseY);
+
+  stroke(224, 115, 71);
+  fill(255, 246, 211);
+  ellipse(250, 200, 300, 300);
+translate(mouseX, mouseY);
+  stroke(255, 255, 255);
+  fill(224, 115, 71);
+  ellipse(180, 200, 250, 250);
+
+
+stroke(224, 115, 71);
+  fill(0);
+  ellipse(180, 200, 200, 200);
+
+}
